87557b5a83f7276501d0b69f6180ded6c95ec938
renzhulei@neotrident.com <renzhulei@neotrident.com>
2019-03-12 16:44:18 +0800

    first

---
+++ js/bootstrap-table/dist/locale/bootstrap-table-cs-I18N.js
@@ -0,0 +1,78 @@
+﻿/**
+ * Bootstrap Table Chinese translation
+ * Author: Zhixin Wen<wenzhixin2010@gmail.com>
+ */
+$(function () {
+    $.fn.bootstrapTable.locales[i18n.locale] = {
+        formatLoadingMessage: function () {
+            if (i18n.locale == "zh")
+                return '正在努力地加载数据中，请稍候……';
+            else
+                return 'Loading, please wait...';
+        },
+        formatRecordsPerPage: function (pageNumber) {
+            if (i18n.locale == "zh")
+                return '每页显示 ' + pageNumber + ' 条记录';
+            else
+                return pageNumber + ' rows per page';
+        },
+        formatShowingRows: function (pageFrom, pageTo, totalRows) {
+            if (i18n.locale == "zh")
+                return '显示第 ' + pageFrom + ' 到第 ' + pageTo + ' 条记录，总共 ' + totalRows + ' 条记录';
+            else
+                return 'Showing ' + pageFrom + ' to ' + pageTo + ' of ' + totalRows + ' rows';
+        },
+        formatSearch: function () {
+            if (i18n.locale == "zh")
+                return '搜索';
+            else
+                return 'Search';
+        },
+        formatNoMatches: function () {
+            if (i18n.locale == "zh")
+                return '没有找到匹配的记录';
+            else
+                return 'No matching records found';
+        },
+        formatPaginationSwitch: function () {
+            if (i18n.locale == "zh")
+                return '隐藏/显示分页';
+            else
+                return 'Hide/Show pagination';
+        },
+        formatRefresh: function () {
+            if (i18n.locale == "zh")
+                return '刷新';
+            else
+                return 'Refresh';
+        },
+        formatToggle: function () {
+            if (i18n.locale == "zh")
+                return '切换';
+            else
+                return 'Toggle';
+        },
+        formatColumns: function () {
+            if (i18n.locale == "zh")
+                return '列';
+            else
+                return 'Columns';
+        },
+        formatExport: function () {
+            if (i18n.locale == "zh")
+                return '导出数据';
+            else
+                return 'Export data';
+
+        },
+        formatClearFilters: function () {
+            if (i18n.locale == "zh")
+                return '清空过滤';
+            else
+                return 'Clear filters';
+        }
+    };
+
+    $.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales[i18n.locale]);
+
+});
---
+++ js/i18n.js
@@ -0,0 +1,73 @@
+﻿
+
+//语言
+var language;
+//国际化控件
+var i18n;
+
+//ECharts图形控件
+var Charts = [];
+
+//Plots图形控件
+var Plots = [];
+
+
+var vm;
+
+//参数
+var parameter = {
+    language: "language",  //语言记录Cookie
+    VueId: "#vue_app" //Vue绑定
+}
+
+
+
+$(function () {
+    //获取本地语言包
+    //异步获取会没有response，需要暂时关闭异步
+    $.ajaxSettings.async = false;
+    var dictions = {
+        en: $.getJSON('js/languages/string_en.json').responseJSON,
+        zh: $.getJSON('js/languages/string_zh.json').responseJSON
+    }
+    $.ajaxSettings.async = true;
+    //如果当前Cookie中没有语言记录，默认为中文
+    if ($.cookie(parameter.language) == null) {
+        $.cookie(parameter.language, 'zh', { expires: 7, path: '/' });
+        language = "zh";
+    }
+    else {
+        language = $.cookie(parameter.language);
+    }
+    i18n = new VueI18n({
+        locale: language, // 设置语言
+        messages: dictions, // 设置字典
+    })
+
+    vm = new Vue({
+        i18n: i18n,
+        data() {
+            return {
+                ReloadFlag: true
+            }
+        }
+    }).$mount(parameter.VueId);
+})
+
+//语言编辑
+function switchLang() {
+    i18n.locale = (i18n.locale == "en" ? "zh" : "en");
+    $.cookie(parameter.language, i18n.locale, { expires: 7, path: '/' });
+
+    location.reload();
+    return;
+}
+
+//语言编辑
+function choiseLang(lang) {
+    i18n.locale = lang;
+    moment.locale(lang);
+    $.cookie(parameter.language, i18n.locale, { expires: 7, path: '/' });
+    vm.rerender();
+}
+
